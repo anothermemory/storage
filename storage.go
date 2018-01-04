e5f018ac7b41735fd8f174586d832ddcae6b698a
Vyacheslav Enis <venis@difane.com>
2018-01-04 19:56:21 +0200

    Added storage interface

---
+++ storage.go
@@ -0,0 +1,26 @@
+package storage
+
+import (
+	"github.com/anothermemory/unit"
+)
+
+// Storage represents some storage which can be used to store units
+type Storage interface {
+	// SaveUnit saves given unit to the storage.
+	SaveUnit(u unit.Unit) error
+
+	// RemoveUnit removes given unit from the storage.
+	RemoveUnit(u unit.Unit) error
+
+	// LoadUnit loads unit with given id from the storage
+	LoadUnit(id string) (unit.Unit, error)
+
+	// IsCreated checks whether storage was already created or not
+	IsCreated() bool
+
+	// Create creates storage
+	Create() error
+
+	// Remove removes storage
+	Remove() error
+}
